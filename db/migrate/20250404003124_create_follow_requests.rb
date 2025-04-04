3a469e3a55aebf76dfbfca3d2d00bbdf28bf88be
kkwon35 <142443004+kkwon35@users.noreply.github.com>
2025-04-04 00:52:01 +0000

    add column to users table with photos_count column and integer datatype

--- db/migrate/20250404003124_create_follow_requests.rb
+++ db/migrate/20250404003124_create_follow_requests.rb
@@ -9,3 +9,9 @@ class CreateFollowRequests < ActiveRecord::Migration[8.0]
     end
   end
 end
+
+class AddPhotosCountToUsers < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :photos_count, :integer, default: 0
+  end
+end
