b142111ad7c9b638e4da95c40725cc2d183061fe
kkwon35 <142443004+kkwon35@users.noreply.github.com>
2025-04-05 22:54:14 +0000

    Added own photos to users#show

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  mount_uploader :avatar_image, ImageUploader
 
   has_many :own_photos, class_name: "Photo", foreign_key: :owner_id
   has_many :comments, foreign_key: "author_id"
